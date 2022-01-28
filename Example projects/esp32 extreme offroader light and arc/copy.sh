4eb3992090560959f1f57149eaea22e5e35176bf
Anton Vanhoucke <antonvh@gmail.com>
2022-01-28 18:58:51 +0100

    Updated copy shell script

--- Example projects/esp32 extreme offroader light and arc/copy.sh
+++ Example projects/esp32 extreme offroader light and arc/copy.sh
@@ -1,3 +1,6 @@
+# Copy library and main.py to esp on Macos.
+# On linux you can simply connect to /dev/ttyAMA0
+
 USB=$(ls /dev/tty* | grep usb)
-rshell -p $USB "cp -r mpy_robot_tools /pyboard"
+rshell -p $USB "cp -r ../projects/mpy_robot_tools /pyboard"
 rshell -p $USB "cp main.py /pyboard"
