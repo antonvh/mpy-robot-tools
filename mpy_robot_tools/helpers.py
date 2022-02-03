66bd834df05242ab7c243395d7506972b0d7ff58
Anton Vanhoucke <antonvh@gmail.com>
2022-02-03 18:32:50 +0100

    add motor stub

--- mpy_robot_tools/helpers.py
+++ mpy_robot_tools/helpers.py
@@ -27,9 +27,25 @@ def scale(val, src, dst):
     """
     return (float(val - src[0]) / (src[1] - src[0])) * (dst[1] - dst[0]) + dst[0]
 
-__MSHUB = const(0)
-__PYBRICKS = const(1)
-__STUB = const(3)
+
+class MotorStub():
+    __angle = 0
+
+    def dc(self, n):
+        self.dc = n
+
+    def angle(self):
+        return self.__angle
+
+    def reset_angle(self, *args):
+        if args:
+            self.__angle = args[0]
+        else:
+            self.__angle = 0
+
+    def track_target(self, t):
+        self.__angle = t
+    
 
 class PBMotor():
     """
@@ -38,21 +54,28 @@ class PBMotor():
     this class takes any motor type object as parameter
     and runs it pybricks-style, with the pybricks motor methods
     """
+    __MSHUB = const(0)
+    __PYBRICKS = const(1)
+    
     def __init__(self, motor):
         motor_dir = dir(motor)
         if '_motor_wrapper' in motor_dir:
             self.motor = motor._motor_wrapper.motor
-            self.type = __MSHUB
+            self.kind = self.__MSHUB
         elif 'get' in motor_dir:
             self.motor = motor
-            self.type = __MSHUB
+            self.kind = self.__MSHUB
         elif 'run_angle' in motor_dir:
             self.motor = motor
-            self.type = __PYBRICKS
+            self.kind = self.__PYBRICKS
         elif 'upper' in motor_dir:
             # We have a string
-            self.type = __MSHUB
-            self.motor = eval("port."+motor+".motor")
+            if motor in 'ABCDEFGH':
+                self.kind = self.__MSHUB
+                self.motor = eval("port."+motor+".motor")
+            else:
+                self.kind = 1
+                self.motor = MotorStub()
         else:
             print("Unknown motor type")
             # We should probably raise an IOerror here
@@ -60,24 +83,31 @@ class PBMotor():
         self.timer = Timer()
 
     def dc(self, duty):
-        if self.type == __MSHUB:
+        if self.kind == self.__MSHUB:
             self.motor.pwm(clamp_int(duty))
-        elif self.type == __PYBRICKS:
+        elif self.kind == self.__PYBRICKS:
             self.motor.dc(duty)
 
     def abs_angle(self):
         return self.motor.get()[2]
 
     def angle(self):
-        if self.type == __MSHUB:
+        if self.kind == self.__MSHUB:
             return self.motor.get()[1]
-        elif self.type == __PYBRICKS:
-            self.motor.angle()
+        elif self.kind == self.__PYBRICKS:
+            return self.motor.angle()
+
+    def servo_pwm(self):
+        return clamp_int(scale(
+            self.motor.angle(), 
+            (-90, 90),
+            (40, 115)
+            ), 40, 115)
 
     def reset_angle(self, *args):
         # Pass 0 to set current position to zero
         # Without arguments this resets to the absolute encoder position
-        if self.type == __MSHUB:
+        if self.kind == self.__MSHUB:
             if len(args) == 0:
                 absolute_position = self.motor.get()[2]
                 if absolute_position > 180:
@@ -85,11 +115,11 @@ class PBMotor():
                 self.motor.preset(absolute_position)
             else:
                 self.motor.preset(args[0])
-        elif self.type == __PYBRICKS:
+        elif self.kind == self.__PYBRICKS:
             self.motor.reset_angle(*args)
 
     def track_target(self, target, gain=1.5):
-        if self.type == __MSHUB:
+        if self.kind == self.__MSHUB:
             # If track target isn't called again within 500ms, fall back to run_to_position
             self.timer.init(
                 mode=Timer.ONE_SHOT, 
@@ -97,5 +127,5 @@ class PBMotor():
                 callback=lambda x: self.motor.run_to_position(target))
             track_target(self.motor, target, gain)
             
-        elif self.type == __PYBRICKS:
+        elif self.kind == self.__PYBRICKS:
             self.motor.track_target(target)
