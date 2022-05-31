17fad5cc121b2824240fae11cea677b9b3b02b44
Humberto Rodríguez A <rhumbertgz@users.noreply.github.com>
2022-05-31 15:51:34 +0200

    Fix PEP warnings about missing whitespaces

--- Installer/base_script.py
+++ Installer/base_script.py
@@ -9,13 +9,14 @@ from ubinascii import hexlify, a2b_base64
 from uhashlib import sha256
 from os import mkdir
 
+encoded = {}
 
-encoded={}
 
 def calc_hash(b):
     return hexlify(sha256(b).digest()).decode()
 
-error=False
+
+error = False
 try:
     mkdir('/projects/mpy_robot_tools')
 except:
@@ -24,27 +25,26 @@ except:
 for file, code, hash_gen in encoded:
     print("Writing file ", file)
     # hash_gen=code[1]
-    target_loc = '/projects/mpy_robot_tools/'+file
-    
-    print('writing '+file+' to folder /projects/mpy_robot_tools')
-    with open(target_loc,'wb') as f:
+    target_loc = '/projects/mpy_robot_tools/' + file
+
+    print('writing ' + file + ' to folder /projects/mpy_robot_tools')
+    with open(target_loc, 'wb') as f:
         for chunk in code:
             f.write(a2b_base64(chunk))
     del code
 
     try:
-        print('Finished writing '+file+', Checking hash.')
-        result=open(target_loc,'rb').read()
-        hash_check=calc_hash(result)
+        print('Finished writing ' + file + ', Checking hash.')
+        result = open(target_loc, 'rb').read()
+        hash_check = calc_hash(result)
 
-        print('Hash generated: ',hash_gen)
+        print('Hash generated: ', hash_gen)
 
         if hash_check != hash_gen:
-            print('Failed hash of .mpy on SPIKE: '+hash_check)
-            error=True
+            print('Failed hash of .mpy on SPIKE: ' + hash_check)
+            error = True
     except Exception as e:
         print(e)
-
 
 if not error:
     print('Library written succesfully. Reset your hub.')
