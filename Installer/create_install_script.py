e03d9d54da90176355b60fe0082b264e2f3f4712
Humberto Rodríguez A <rhumbertgz@users.noreply.github.com>
2022-05-31 15:50:02 +0200

    Fix PEP warnings about missing whitespaces

--- Installer/create_install_script.py
+++ Installer/create_install_script.py
@@ -16,31 +16,30 @@ MPY_LIB = 'mpy/'
 INSTALLER = 'install_mpy_robot_tools.py'
 BASE_SCRIPT = 'base_script.py'
 SKIP_FILES = ['__pycache__', 'servo.py', 'hub_stub.py', 'np_animation.py']
-CHUNK_SIZE = 2**8
+CHUNK_SIZE = 2 ** 8
 
 files = [f for f in os.listdir(LIB) if f not in SKIP_FILES]
 encoded = []
 
 for f in files:
-    out_file = f.split(".")[0]+".mpy"
-    out_file_loc = MPY_LIB+out_file
-    mpy_cross.run('-march=armv6',LIB+f,'-o', out_file_loc)
+    out_file = f.split(".")[0] + ".mpy"
+    out_file_loc = MPY_LIB + out_file
+    mpy_cross.run('-march=armv6', LIB + f, '-o', out_file_loc)
     time.sleep(0.5)
-    with open(out_file_loc,'rb') as mpy_file:
+    with open(out_file_loc, 'rb') as mpy_file:
         file_hash = hashlib.sha256(mpy_file.read()).hexdigest()
     chunks = []
-    with open(out_file_loc,'rb') as mpy_file:
+    with open(out_file_loc, 'rb') as mpy_file:
         for chunk in iter(partial(mpy_file.read, CHUNK_SIZE), b''):
             chunks += [binascii.b2a_base64(chunk).decode('utf-8')]
-    print(out_file,": ",len(chunks)," chunks of ", CHUNK_SIZE)
+    print(out_file, ": ", len(chunks), " chunks of ", CHUNK_SIZE)
     encoded += [(
         out_file,
-        tuple(chunks), 
+        tuple(chunks),
         file_hash
     )]
 
+spike_code = open(BASE_SCRIPT, 'r').read()
 
-spike_code=open(BASE_SCRIPT,'r').read()
-
-with open(INSTALLER,'w') as f:
-     f.write(spike_code.format(repr(tuple(encoded))))
+with open(INSTALLER, 'w') as f:
+    f.write(spike_code.format(repr(tuple(encoded))))
