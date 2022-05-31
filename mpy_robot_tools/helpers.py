106dcb3e36b1c2fd898481120921ef1bda22cef8
Humberto Rodríguez A <rhumbertgz@users.noreply.github.com>
2022-05-31 18:59:29 +0200

    Update 'scale' function docs

--- mpy_robot_tools/helpers.py
+++ mpy_robot_tools/helpers.py
@@ -21,14 +21,18 @@ def track_target(motor, target=0, gain=1.5):
 
 
 def scale(val, src, dst):
-    """
-    Scale the given value from the scale of src to the scale of dst.
+    """Scales the given value from the scale of src to the scale of dst.
 
-    val: float or int
-    src: tuple
-    dst: tuple
+    Args:
+        val (float | int): ....
+        src (tuple): ....
+        dst (tuple)
 
-    example: print scale(99, (0.0, 99.0), (-1.0, +1.0))
+    Return:
+        Scaled value.
+
+    Example:
+        print scale(99, (0.0, 99.0), (-1.0, +1.0))
     """
     return (float(val - src[0]) / (src[1] - src[0])) * (dst[1] - dst[0]) + dst[0]
 
