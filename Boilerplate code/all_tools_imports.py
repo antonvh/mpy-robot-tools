a41bc7bc0cb17f7e1e3db37cf6116f0b7d94e4fe
Anton Vanhoucke <antonvh@gmail.com>
2022-07-12 18:03:05 +0200

    Fixed boilerplate

--- Boilerplate code/all_tools_imports.py
+++ Boilerplate code/all_tools_imports.py
@@ -1,15 +1,16 @@
-from projects.mpy_robot_tools.bt import UARTPeripheral, UARTCentral, BLEHandler, CONNECT_IMAGES
+from projects.mpy_robot_tools.bt import UARTPeripheral, UARTCentral, BLEHandler
 from projects.mpy_robot_tools.rc import (
     RCReceiver, RCTransmitter, 
     L_STICK_HOR, L_STICK_VER, R_STICK_HOR, R_STICK_VER,
-    L_TRIGGER, R_TRIGGER, SETTING1, SETTING2, BUTTONS
+    L_TRIGGER, R_TRIGGER, SETTING1, SETTING2, BUTTONS, 
+    CONNECT_IMAGES
 )
 # uartremote is deprepecated. Use SerialTalk.
 # from projects.mpy_robot_tools.uartremote import *
 from projects.mpy_robot_tools.serialtalk import SerialTalk
 from projects.mpy_robot_tools.mshub import MSHubSerial
 from projects.mpy_robot_tools.ctrl_plus import SmartHub
-from mpy_robot_tools.light import codelines, LMAnimation
+from projects.mpy_robot_tools.light import codelines, LMAnimation
 from projects.mpy_robot_tools.motor_sync import Mechanism, AMHTimer, sine_wave, linear_interpolation, linear, block_wave
 from projects.mpy_robot_tools.helpers import clamp_int, track_target, scale, PBMotor, PBUltrasonicSensor
 from projects.mpy_robot_tools.pyhuskylens import (
