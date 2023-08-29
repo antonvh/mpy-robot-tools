8ddb22a6646c8f1c39b1c3f41d8e744b4fd9268f
Anton Vanhoucke <antonvh@gmail.com>
2023-08-30 01:07:36 +0200

    fix broken test

--- Tests/test_helpers.py
+++ Tests/test_helpers.py
@@ -1,10 +1,9 @@
-from mpy_robot_tools.helpers import PBMotor
+from mpy_robot_tools.pybricks import Motor
 
 def test_pbmotor():
-    m = PBMotor('stub')
+    m = Motor('stub')
     assert m.angle() == 0
     m.reset_angle(50)
     assert m.angle() == 50
     m.track_target(90)
     assert m.angle() == 90
-    assert m.servo_pwm() == 115
