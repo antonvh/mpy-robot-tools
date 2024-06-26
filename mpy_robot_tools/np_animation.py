7715f2da7ef82fa54933fce6e0d4a3fdfeae7ec5
Anton Vanhoucke <antonvh@gmail.com>
2024-06-26 15:14:38 +0200

    put colors in enumartor to avoid clash with ctrl_plus

--- mpy_robot_tools/np_animation.py
+++ mpy_robot_tools/np_animation.py
@@ -4,7 +4,7 @@
 from neopixel import NeoPixel
 from machine import Pin
 import utime
-from math import sin, pi
+from math import pi
 
 def clamp(value, min_value, max_value):
     return max(min_value, min(max_value, value))
@@ -61,15 +61,39 @@ def to_grb(rgb):
 def from_grb(grb):
     return grb[1],grb[0],grb[2]
 
-# Neopixel has GRB colors, not rgb !
-ORANGE = b'\x66\xfc\x03'
-OFF = b'\x00\x00\x00'
-WHITE = b'\xff\xff\xff'
-RED = b'\x00\xff\x00'
-DARK_RED = b'\x00\x44\x00'
-BLUE = b'\x00\x00\xff'
 
-def indicators(on=ORANGE, off=OFF, interval=500, name="indicators"):
+
+class grb():
+    # Enum. Neopixel has GRB colors in it's buffer, not rgb !
+    ORANGE = b'\x66\xfc\x03'
+    BLACK = NONE = OFF = b'\x00\x00\x00'
+    WHITE = b'\xff\xff\xff'
+    RED = b'\x00\xff\x00'
+    DARK_RED = b'\x00\x44\x00'
+    BLUE = b'\x00\x00\xff'
+    YELLOW = b'\xff\xff\x00' #(255,255,0),
+    GREEN = b'\xff\x00\x00' # (0,255,0),
+    CYAN = b'\xff\x00\xff' #: (0,255,255),
+    VIOLET = b'\x7f\x7f\xff' #: (127,127,255),
+    MAGENTA = b'\x00\xff\xff' #: (255,0,255),
+    GRAY = b'\x7f\x7f\x7f' #: (127,127,127),
+
+class rgb():
+    ORANGE = from_grb(grb.ORANGE)
+    BLACK = NONE = OFF = from_grb(grb.BLACK)
+    WHITE = from_grb(grb.WHITE)
+    RED = from_grb(grb.RED)
+    DARK_RED = from_grb(grb.DARK_RED)
+    BLUE = from_grb(grb.BLUE)
+    YELLOW = from_grb(grb.YELLOW)
+    GREEN = from_grb(grb.GREEN)
+    CYAN = from_grb(grb.CYAN)
+    VIOLET = from_grb(grb.VIOLET)
+    MAGENTA = from_grb(grb.MAGENTA)
+    GRAY = from_grb(grb.GRAY)
+
+
+def indicators(on=grb.ORANGE, off=grb.OFF, interval=500, name="indicators"):
     def func(time, **kwargs):
         switch=True
         if name in kwargs:
@@ -77,12 +101,12 @@ def indicators(on=ORANGE, off=OFF, interval=500, name="indicators"):
         return on if (time % (interval*2) < interval) and switch else off
     return func
 
-def indicators_right(on=ORANGE, off=OFF, interval=500):
+def indicators_right(on=grb.ORANGE, off=grb.OFF, interval=500):
     def func(time, turn=0, **kwargs):
         return on if (time % (interval*2) < interval) and turn > 0 else off
     return func
 
-def brake_lights(drive=DARK_RED, brake=RED, reverse=WHITE):
+def brake_lights(drive=grb.DARK_RED, brake=grb.RED, reverse=grb.WHITE):
     def func(time, speed=0, **kwargs):
         if speed < 0:
             return reverse
@@ -92,7 +116,7 @@ def brake_lights(drive=DARK_RED, brake=RED, reverse=WHITE):
             return brake
     return func
 
-def switch(on=WHITE, off=OFF, name="switch"):
+def switch(on=grb.WHITE, off=grb.OFF, name="switch"):
     def func(time, **kwargs):
         switch=True
         if name in kwargs:
@@ -100,7 +124,7 @@ def switch(on=WHITE, off=OFF, name="switch"):
         return on if switch else off
     return func
 
-def delayed_switch(on=RED, off=OFF, delay=2000):
+def delayed_switch(on=grb.RED, off=grb.OFF, delay=2000):
     def func(time, **kwargs):
         return on if time < delay else off
     return func
@@ -125,7 +149,7 @@ def knight_rider(period=2000, width=6):
     r_strip.reverse()
     def func(time, **kwargs):
         n = round((time % period)/period * width*2)
-        result = [OFF]*width
+        result = [grb.OFF]*width
         for i in range(width):
             result[i] = bytes((0,max(rotate(strip,n)[i], rotate(r_strip,-n)[i]),0))
         return result
@@ -138,26 +162,26 @@ def knight_rider_gen(period=2000, width=6):
     r_strip.reverse()
     n = 0
     while n<=2*width:
-        result = [OFF]*width
+        result = [grb.OFF]*width
         for i in range(width):
             result[i] = bytes((0,max(rotate(strip,n)[i], rotate(r_strip,-n)[i]),0))
         yield int(period/(2*width)*n), result
         n+=1
 
 EMERGENCY_1 = [
-    (0,     [RED]*3+[OFF]*3),
-    (150,   [OFF]*6),
-    (200,   [RED]*3+[OFF]*3),
-    (350,   [OFF]*6),
-    (400,   [RED]*3+[OFF]*3),
-    (450,   [OFF]*6),
-    (500,  [OFF]*3+[BLUE]*3),
-    (650,  [OFF]*6),
-    (700,  [OFF]*3+[BLUE]*3),
-    (850,  [OFF]*6),
-    (900,  [OFF]*3+[BLUE]*3),
-    (1050,  [OFF]*6),
-    (1100,  [OFF]*6)
+    (0,     [grb.RED]*3+[grb.OFF]*3),
+    (150,   [grb.OFF]*6),
+    (200,   [grb.RED]*3+[grb.OFF]*3),
+    (350,   [grb.OFF]*6),
+    (400,   [grb.RED]*3+[grb.OFF]*3),
+    (450,   [grb.OFF]*6),
+    (500,  [grb.OFF]*3+[grb.BLUE]*3),
+    (650,  [grb.OFF]*6),
+    (700,  [grb.OFF]*3+[grb.BLUE]*3),
+    (850,  [grb.OFF]*6),
+    (900,  [grb.OFF]*3+[grb.BLUE]*3),
+    (1050,  [grb.OFF]*6),
+    (1100,  [grb.OFF]*6)
 ]
 
 def keyframes(frames):
@@ -165,7 +189,7 @@ def keyframes(frames):
     period = frames[0][0]
     if period == 0: period = 1
     def func(time, **kwargs):
-        result = [OFF]
+        result = [grb.OFF]
         for frame_time, pixels in frames:
             if time%period >= frame_time:
                 result = pixels
@@ -177,7 +201,7 @@ def keyframes_dict(frames_dict, name="animation"):
     for k in frames_dict:
         frames_dict[k].reverse()
     def func(time, **kwargs):
-        result = [OFF]
+        result = [grb.OFF]
         if name in kwargs:
             anim_key = kwargs[name]
             if anim_key in frames_dict:
